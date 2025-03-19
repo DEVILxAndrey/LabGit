172893e907cb2a5d587e64fb7995fe79ff8b78c1
Лиза <elizavetaborisova879@gmil.com>
2025-03-19 09:35:06 +0700

    laba

--- Прямоугольник.cpp
+++ Прямоугольник.cpp
@@ -9,8 +9,17 @@ int main() {
     // Запросить размеры прямоугольника
     std::cout << "Введите ширину прямоугольника: ";
     std::cin >> width;
+    while (width <= 0) {
+        std::cout << "Ширина должна быть положительной. Введите ширину заново: ";
+        std::cin >> width;
+    }
+
     std::cout << "Введите длину прямоугольника: ";
     std::cin >> height;
+    while (height <= 0) {
+        std::cout << "Длина должна быть положительной. Введите длину заново: ";
+        std::cin >> height;
+    }
 
     // Вычислить периметр
     double perimeter = 2 * (width + height);
@@ -23,7 +32,7 @@ int main() {
 
     // Вывести результаты
     std::cout << "Периметр прямоугольника: " << perimeter << std::endl;
-    std::cout << "Площадь прямоугольника1: " << area << std::endl;
+    std::cout << "Площадь прямоугольника: " << area << std::endl;
     std::cout << "Длина диагонали прямоугольника: " << diagonal << std::endl;
  
     return 0;
